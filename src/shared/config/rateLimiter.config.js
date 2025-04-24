bada17078cc7b6a0ea66bd68d563e35813371ac9
diaochan <diaochan@seatent.com>
2025-04-24 15:37:29 +0800

    Revert "接口限流"
    This reverts commit 0d0a699cc274348ee94ede3639b46ba44389e761.

--- src/shared/config/rateLimiter.config.js
+++ src/shared/config/rateLimiter.config.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-03-30 19:56:58
  * @LastEditors: diaochan
- * @LastEditTime: 2025-04-24 15:31:46
+ * @LastEditTime: 2025-04-22 19:45:31
  * @Description: 
  */
 /**
@@ -13,7 +13,7 @@
 // 通用API限流配置
 const apiLimiterConfig = {
   windowMs: 15 * 60 * 1000,                     // 时间窗口，默认15分钟
-  max: 800,                                     // 在windowMs内允许的最大请求数
+  max: 500000,                                     // 在windowMs内允许的最大请求数
   standardHeaders: true,                        // 返回标准的RateLimit头部信息
   legacyHeaders: false                          // 禁用旧版头部
 };
@@ -21,7 +21,7 @@ const apiLimiterConfig = {
 // 登录接口限流配置（更严格）
 const loginLimiterConfig = {
   windowMs: 60 * 60 * 1000,                     // 时间窗口，1小时
-  max: 200,                                      // 在windowMs内允许的最大请求数
+  max: 200000,                                      // 在windowMs内允许的最大请求数
   standardHeaders: true,                        // 返回标准的RateLimit头部信息
   legacyHeaders: false                          // 禁用旧版头部
 };
