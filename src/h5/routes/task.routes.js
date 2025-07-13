13dbba88564c3043dedd2e49f0e2d87db8e828d2
diaochan <diaochan@seatent.com>
2025-07-13 17:37:29 +0800

    移除不需要的接口

--- src/h5/routes/task.routes.js
+++ src/h5/routes/task.routes.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-03-25 10:15:13
  * @LastEditors: diaochan
- * @LastEditTime: 2025-04-18 16:04:51
+ * @LastEditTime: 2025-07-13 17:31:18
  * @Description: 
  */
 /**
@@ -50,65 +50,7 @@ router.get(
   taskController.getList
 );
 
-/**
- * @route GET /api/h5/tasks/all
- * @desc 获取所有任务列表（不筛选状态）
- * @param {number[]} taskIds - 任务ID数组，格式：taskIds[]=1&taskIds[]=2&taskIds[]=3
- * @param {number} page - 页码
- * @param {number} pageSize - 每页数量
- * @param {number} channelId - 频道ID筛选
- * @param {string} category - 分类筛选
- * @access Private
- */
-router.get(
-  '/all',
-  [
-    query('page')
-      .optional()
-      .isInt({ min: 1 })
-      .withMessage('common.validation.page'),
-    query('pageSize')
-      .optional()
-      .isInt({ min: 1 })
-      .withMessage('common.validation.pageSize'),
-    query('channelId')
-      .optional()
-      .isInt()
-      .withMessage('common.validation.mustBeInt'),
-    query('category')
-      .optional()
-      .isString()
-      .withMessage('common.validation.mustBeString'),
-    query('taskIds')
-      .optional()
-      .custom((value) => {
-        if (!value) return true;
-        
-        // 仅支持数组格式：taskIds[]=1&taskIds[]=2&taskIds[]=3
-        if (!Array.isArray(value)) {
-          throw new Error('taskIds must be an array');
-        }
-        
-        // 验证数组中的每个元素都是正整数
-        if (value.length > 0) {
-          const isValid = value.every(id => {
-            const num = parseInt(id, 10);
-            return Number.isInteger(num) && num > 0;
-          });
-          
-          if (!isValid) {
-            throw new Error('taskIds must contain only positive integers');
-          }
-        }
-        
-        return true;
-      })
-      .withMessage('common.validation.taskIds')
-  ],
-  (req, res, next) => validatorUtil.validateRequest(req, res) ? next() : null,
-  authMiddleware.optionalVerifyToken,
-  taskController.getAllList
-);
+
 
 /**
  * @route GET /api/h5/tasks/:id
