e81792fc8a13c48f02b236f350cca0dfc8a006fd
diaochan <diaochan@seatent.com>
2025-07-24 20:18:00 +0800

    图片上传

--- src/shared/controllers/upload.controller.js
+++ src/shared/controllers/upload.controller.js
@@ -10,16 +10,17 @@ const ossUtil = require('../utils/oss.util');
  * 上传图片
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
+ * @body {string} [directory] - OSS自定义目录
  */
 async function uploadImage(req, res) {
   try {
     if (!req.file) {
       return responseUtil.badRequest(res, '请选择要上传的图片');
     }
-    
-    // 使用OSS上传文件
-    const ossUrl = await ossUtil.uploadFile(req.file);
-    
+    // 获取自定义目录参数
+    const directory = req.body.directory;
+    // 使用OSS上传文件，支持自定义目录
+    const ossUrl = await ossUtil.uploadFile(req.file, directory);
     return responseUtil.success(res, { url: ossUrl });
   } catch (error) {
     logger.error(`上传图片失败: ${error.message}`);
